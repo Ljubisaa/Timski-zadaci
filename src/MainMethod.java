cb21f7eb8cae8148572d78cc421f73388a256601
Dino Spreco <dinospreco@gmail.com>
2018-02-09 22:13:12 +0100

    Implemented methods sort() and sumColumn(). Added methods printMatrix() and generateMatrix()

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -1,8 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] matrix;
+		matrix = generateMatrix(5);
+		printMatrix(matrix);
+		System.out.println();
+		sort(matrix);
+		printMatrix(matrix);
+
 	}
 
 	public static int[] eliminateDuplicates(int[] list) {
@@ -21,14 +29,50 @@ public class MainMethod {
 		return sum;
 	}
 	//Stefan
+
 	public static double sumColumn(double[][] m, int columnIndex){
-		double a = 0;
-		return a;
+		double columnSum = 0;
+		for (int i = 0 ; i < m.length ; i++) {
+			columnSum += m[i][columnIndex];
+		}
+		return columnSum;
 	}
 	//Dino
 	public static void sort(int m[][]){
-		
+		int[] tmp;
+		for (int i = 0 ; i < m.length ; i++) {
+			tmp = m[i];
+			Arrays.sort(tmp);
+		}
+		for (int i = 0 ; i < m.length - 1 ; i++) {
+			for (int j = i ; j < m.length ; j++) {
+				if (m[i][0] > m[j][0]) {
+					tmp = m[i];
+					m[i] = m[j];
+					m[j] = tmp;
+				}
+			}
+		}
 	}
 	//Dino
+	//This is for testing purposes only
+	public static int[][] generateMatrix(int numOfRowsAndColumns) {
+		int[][] matrix = new int[numOfRowsAndColumns][numOfRowsAndColumns];
+		for (int i = 0 ; i < numOfRowsAndColumns ; i++) {
+			for (int j = 0 ; j < numOfRowsAndColumns ; j++) {
+				matrix[i][j] = (int)(Math.random()*100);
+			}
+		}
+		return matrix;
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0 ; i < matrix.length ; i++) {
+			for (int j = 0 ; j < matrix[i].length ; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 
 }
