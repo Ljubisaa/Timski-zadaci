35ffc3d3243f7ac39386d3c6c97ab6176b1adbc3
Ljubisa Savic <ljsavic90@gmail.com>
2018-02-09 11:17:42 +0100

    Osnovna verzija

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -8,24 +8,27 @@ public class MainMethod {
 	public static int[] eliminateDuplicates(int[] list) {
 		return list;
 	}
-
+    
 	public static boolean isSorted(int[] list) {
 		return true;
 	}
 	public static String sort(String s){
 		return s;
 	}
+	//Stefan
 	public static double sumMajorDiagonal(double[][] m){
 		double sum = 0;
 		return sum;
 	}
+	//Stefan
 	public static double sumColumn(double[][] m, int columnIndex){
 		double a = 0;
 		return a;
 	}
+	//Dino
 	public static void sort(int m[][]){
 		
 	}
-	
+	//Dino
 
 }
