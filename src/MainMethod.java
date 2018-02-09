29638567aa98dd5196d459ad9f8d554facb96147
Ljubisa Savic <ljsavic90@gmail.com>
2018-02-09 11:06:54 +0100

    Osnovna verzija

---
+++ src/MainMethod.java
@@ -0,0 +1,31 @@
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	public static int[] eliminateDuplicates(int[] list) {
+		return list;
+	}
+
+	public static boolean isSorted(int[] list) {
+		return true;
+	}
+	public static String sort(String s){
+		return s;
+	}
+	public static double sumMajorDiagonal(double[][] m){
+		double sum = 0;
+		return sum;
+	}
+	public static double sumColumn(double[][] m, int columnIndex){
+		double a = 0;
+		return a;
+	}
+	public static void sort(int m[][]){
+		
+	}
+	
+
+}
