30738898c916a9b8f388f758d06f6634c07d6b7f
Ljubisa Savic <ljsavic90@gmail.com>
2018-02-09 19:24:12 +0100

    Moje metode

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -2,33 +2,77 @@
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int[] a = { 1, 5, 6, 7, 8, 1};
+
+		if (isSorted(a)) {
+			System.out.println("Niz je sortiran.");
+		} else {
+			System.out.println("Niz nije sortiran.");
+		}
+
+		eliminateDuplicates(a);
+
+		for (int i = 0; i < eliminateDuplicates(a).length; i++) {
+			System.out.print(a[i] + " ");
+		}
+
 	}
 
-	public static int[] eliminateDuplicates(int[] list) {
-		return list;
+	public static int[] eliminateDuplicates(int[] arr) {
+
+		int end = arr.length;
+
+		for (int i = 0; i < end; i++) {
+			for (int j = i + 1; j < end; j++) {
+				if (arr[i] == arr[j]) {
+
+					arr[j] = arr[end - 1];
+					end--;
+					j--;
+				}
+			}
+		}
+
+		int[] whitelist = new int[end];
+
+		System.arraycopy(arr, 0, whitelist, 0, end);
+		return whitelist;
+
 	}
-    
+
 	public static boolean isSorted(int[] list) {
+
+		for (int i = 0; i < list.length - 1; i++) {
+			if (list[i] > list[i + 1]) {
+				return false;
+			}
+
+		}
+
 		return true;
 	}
-	public static String sort(String s){
+
+	public static String sort(String s) {
 		return s;
 	}
-	//Stefan
-	public static double sumMajorDiagonal(double[][] m){
+
+	// Stefan
+	public static double sumMajorDiagonal(double[][] m) {
 		double sum = 0;
 		return sum;
 	}
-	//Stefan
-	public static double sumColumn(double[][] m, int columnIndex){
+
+	// Stefan
+	public static double sumColumn(double[][] m, int columnIndex) {
 		double a = 0;
 		return a;
 	}
-	//Dino
-	public static void sort(int m[][]){
-		
+
+	// Dino
+	public static void sort(int m[][]) {
+
 	}
-	//Dino
+	// Dino
 
 }
